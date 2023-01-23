1d8b0dd5e63bbb8941cd5fd9af3d01dc6e852854
Waquar Mahboob <waquarmahboob4@gmail.com>
2023-01-23 22:44:08 +0530

    Add LocalStorage

--- src/Components/Users/Create/createPageSlice.tsx
+++ src/Components/Users/Create/createPageSlice.tsx
@@ -1,11 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { parse } from "path";
 import { DataType } from "../View";
 export interface CreateState {
   userData: DataType[];
 }
 
 const initialState: CreateState = {
-  userData: [],
+  userData: JSON.parse(localStorage.getItem("userData")??'[]'),
 };
 
 const createPageSlice = createSlice({
@@ -13,6 +14,7 @@ const createPageSlice = createSlice({
   initialState,
   reducers: {
     addUserDetails(state, action: PayloadAction<DataType>) {
+      
       const { key, name, gender, email, mobile, technology, profilePicture } =
         action.payload;
 
@@ -27,9 +29,9 @@ const createPageSlice = createSlice({
       };
       fromData.key=state.userData.length+1;
       state.userData.push(fromData);
+      localStorage.setItem("userData", JSON.stringify(state.userData));
     },
     viewDetails(state){
-
          return state
     }
   },
