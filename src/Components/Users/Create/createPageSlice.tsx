87a6d417e4bd6eaf0a86dc18ab192d2a34c938d8
Waquar Mahboob <waquarmahboob4@gmail.com>
2023-01-23 23:26:25 +0530

    build

--- src/Components/Users/Create/createPageSlice.tsx
+++ src/Components/Users/Create/createPageSlice.tsx
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { parse } from "path";
 import { DataType } from "../View";
 export interface CreateState {
   userData: DataType[];
